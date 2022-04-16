9d996820adad61e476b6cd2aa8404c3850a1c204
KOSTEN <pwned>
2022-04-16 15:24:12 +0300

    update js

--- website/views.py
+++ website/views.py
@@ -135,7 +135,7 @@ def clicker_page(request):
 
     if request.method == 'POST':
         player =  get_object_or_404(CustomUser, username=request.user)
-        player.balance = (request.POST.get('balance'))
+        player.balance += int((request.POST.get('balance')))
         player.save()
     context = {'page': 'clicker'}
     return render(request, 'clicker.html', context)
