f04d0ab393e346e6622037284eef96d721258181
KOSTEN <pwned>
2022-05-27 22:13:37 +0300

    q

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: gunicorn nft-colossus.wsgi --log-file -
-
--- website/apps.py
+++
@@ -1,8 +0,0 @@
-"""Import line."""
-from django.apps import AppConfig
-
-
-class WebsiteConfig(AppConfig):
-    """Class line."""
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'website'
